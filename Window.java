bd0270771d2aab5ab93b93f90bcb411cf9ba3899
Malachi <malapplekey@gmail.com>
2026-04-20 11:47:33 +0930

    Malachi: Formated time to be more readable

--- Window.java
+++ Window.java
@@ -1,13 +1,21 @@
 import javax.swing.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class Window extends JFrame {
 
     public Window() {
         setTitle("Test Window");
-        JLabel label = new JLabel("Text inside window");
+
+        // Create time and format it
+        LocalDateTime timeObj = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss");
+        String formattedTime = timeObj.format(formatter);
+        JLabel label = new JLabel(formattedTime);
         add(label);
 
-        
+        // Widow settings
         setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
